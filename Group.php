fb6c34fd06f434865cde12406eed8a549703de56
cxgreat2014 <fwy1998@gmail.com>
2016-09-10 22:06:42 +0800

    add some js

--- Group.php
+++ Group.php
@@ -15,54 +15,6 @@ $blogtitle = 'oauth2 - 分组授权';
 require $blogpath . 'zb_system/admin/admin_header.php';
 require $blogpath . 'zb_system/admin/admin_top.php';
 ?>
-    <style>
-        input[type=checkbox]:focus, select:focus, a:focus {
-            outline: 0;
-            border-color: rgba(82, 168, 236, 0.8);
-            -webkit-box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.1), 0 0 8px rgba(82, 168, 236, 0.6); /* Safari 4 */
-            -moz-box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.1), 0 0 8px rgba(82, 168, 236, 0.6); /* Firefox 3.6 */
-            box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.1), 0 0 8px rgba(82, 168, 236, 0.6);
-        }
-        tr:hover{
-            background: #ffffdd;
-        }
-        label {
-            -moz-user-select: none; /*火狐*/
-            -webkit-user-select: none; /*webkit浏览器*/
-            -ms-user-select: none; /*IE10*/
-            -khtml-user-select: none; /*早期浏览器*/
-            user-select: none;
-        }
-        tr td:nth-child(3){
-            padding-left: 10px;
-        }
-        p#vtip {
-            display: none;
-            position: absolute;
-            padding: 10px;
-            font-size: 0.8em;
-            background-color: white;
-            border: 1px solid #a6c9e2;
-            -moz-border-radius: 5px;
-            -webkit-border-radius: 5px;
-            z-index: 9999
-        }
-
-        p#vtip #vtipArrow {
-            position: absolute;
-            top: 39px;
-            left: 5px;
-            transform: rotate(180deg);
-        }
-
-        input[type="text"] {
-            width: 100px;
-        }
-
-        select {
-            width: 120px;
-        }
-    </style>
     <div id="divMain">
         <div class="divHeader"><?php echo $blogtitle; ?></div>
         <?php require dirname(__FILE__) . '/class/ToolBar_top.php'; ?>
@@ -148,7 +100,7 @@ EOF;
         var cmdw = "./cmdw.php?t=" + (new Date()).getTime(),
             nid = 0;
         $("td[colspan='4']").attr("colspan", "6");
-        $("#notice").attr("colspan", "7");
+        $("#notice").attr("colspan",$("tr.color1").children().length);
         $(":checkbox").prop("disabled", true);
 
         $(document).on('click', "a.button", function () {
--- main.php
+++ main.php
@@ -81,13 +81,11 @@ require $blogpath . 'zb_system/admin/admin_top.php';
                 echo $str;
                 ?>
                 <tr>
-                    <td colspan="7"></td>
-                    <td>
-                        <center>
-                            <button class="lbt" type="button" onclick="addnewuser()">
-                                新建用户
-                            </button>
-                        </center>
+                    <td colspan="7" class="tdCenter"></td>
+                    <td class="tdCenter">
+                        <button class="lbt" type="button" onclick="addnewuser()">
+                            新建用户
+                        </button>
                     </td>
                 </tbody>
             </table>
@@ -96,41 +94,162 @@ require $blogpath . 'zb_system/admin/admin_top.php';
     <!--<script src="//ajax.aspnetcdn.com/ajax/jQuery/jquery-3.0.0.min.js"></script>-->
     <script type="text/javascript">
         AddHeaderIcon("<?php echo $bloghost . 'zb_users/plugin/oauth2/logo.png';?>");
-        var newnum = 0;
-        function addnewuser() {
-            newnum++;
-            $("[colspan='7']").parent().before('<tr class="color3">' +
-                '<td class="td5 tdCenter">&nbsp;</td>' +//id留空
-                '<td class="td10"><input type="text" id="NewUserName' + newnum.toString() + '"></td>' +
-                '<td class="td10"></td>' +
+        var cmdw = "./cmdw.php?t=" + (new Date()).getTime();
+
+        $(document).on('click', "a.button", function () {
+            var GroupLine = $(this).parent().parent();
+            var tmp;
+            if ($(this).children().attr("alt") == "编辑") {
+                /*$(this).children().attr({"src": "../../../zb_system/image/admin/tick.png", "alt": "保存", "title": "保存"});
+                var child = GroupLine.children();
+                tmp = child.eq(1);
+                tmp.html('<input type="text" style="width: 100px;" value="' + $.trim(tmp.text()) + '"/>');
+                tmp = child.eq(2);
+                TemplateSwitch($.trim(tmp.text()), GroupLine.find(":checkbox"));
+
+                tmp.html($('<select class="edit" style="width: 120px;" size="1" id="NewGroupTemplate$Nid">' +
+                    '<option value="所有阅读权限">所有阅读权限</option>' +
+                    '<option value="自定义">自定义</option>' +
+                    '<option value="游客">游客</option>' +
+                    '<option value="禁止访问">禁止访问</option>' +
+                    '</select>').val($.trim(tmp.text())));*/
+            } else if ($(this).children().attr("alt") == "提交" || $(this).children().attr("alt") == "保存") {
+                tmp = GroupLine.children();
+                if (tmp.eq(1).children().eq(0).val() == "") {
+                    ShowTip(tmp.eq(1).children().eq(0));
+                    return;
+                }
+                var json = {};
+                if (tmp.eq(0).text() == "") {
+                    json.action = "CreatUser";
+                } else {
+                    json.action = "UpdateUser";
+                    json.gid = tmp.eq(0).text();
+                }
+                json.gname = tmp.eq(1).children().eq(0).val();
+                json.gtemplate = tmp.eq(2).children().eq(0).val();
+                json.gspy = tmp.eq(3).children().eq(0).prop("checked");
+                for (var i = 4; i < (tmp.length - 1); i++) {
+                    json["cl" + (i - 3)] = tmp.eq(i).children().eq(0).prop("checked");
+                }
+                $.ajax({
+                    url: cmdw,
+                    type: "POST",
+                    data: json,
+                    dataType: "json",
+                    success: function (data) {
+                        if (!data.status) {
+                            alert(data.reason);
+                            return;
+                        }
+                        GroupLine.fadeOut("slow", function () {
+                            tmp.eq(0).html(data.gid);
+                            tmp.eq(1).html(tmp.eq(1).children().eq(0).val());
+                            tmp.eq(2).html(tmp.eq(2).children().eq(0).val());
+                            GroupLine.find(":checkbox").prop("disabled", true);
+                            GroupLine.find('img[alt="提交"],img[alt="保存"]').attr({
+                                "src": "../../../zb_system/image/admin/page_edit.png",
+                                "alt": "编辑",
+                                "title": "编辑"
+                            });
+                            if (GroupLine.find('[id^="GroupName"]').length > 0) {
+                                GroupLine.removeAttr("style").fadeIn("slow");
+                            } else {
+                                GroupLine.removeAttr("style").insertBefore($("tr:last"));
+                            }
+
+                        });
+                    }
+                });
+            } else {
+                if (GroupLine.children().first().text() != "") {
+                    $.ajax({
+                        url: cmdw,
+                        type: "POST",
+                        data: {
+                            action: "DelGroup",
+                            gid: GroupLine.children().first().text()
+                        },
+                        dataType: "json",
+                        success: function (data) {
+                            if (data.status) {
+                                GroupLine.fadeOut("slow", function () {
+                                    eval(data.script);
+                                    GroupLine.remove();
+                                });
+                            } else {
+                                alert("删除失败,详情请见控制台");
+                                console.log(data);
+                            }
+                        },
+                        error: function (XMLHttpRequest, textStatus, errorThrown) {
+                            alert("删除请求失败,详情请见控制台");
+                            console.log(XMLHttpRequest.status);
+                            console.log(XMLHttpRequest.readyState);
+                            console.log(textStatus);
+                        }
+                    })
+                } else {
+                    GroupLine.remove();
+                }
+            }
+        });
+
+        function ShowTip(ipt) {
+            $('body').append('<p id="vtip"><img id="vtipArrow" src="./image/vtip_arrow.png"/><img src="./image/inform.png" style="position: absolute;top: 12px;left: 8px;">' + "该字段您还未填写哟~" + '</p>');
+            $('p#vtip').css({
+                "top": (ipt.offset().top - ipt.outerHeight() - 24) + "px",
+                "left": ipt.offset().left + "px",
+                "padding-left": "31px",
+                "padding-right": "11px"
+            }).fadeIn("slow");
+            setTimeout(function () {
+                $('p#vtip').fadeOut("slow", function () {
+                    $('p#vtip').remove();
+                });
+            }, 6000);
+        }
+
+        function addNewUser() {
+            $('tr:last').before('<tr class="color3">' +
+                '<td class="td5 tdCenter">&nbsp;</td>' +
+                '<td class="td10"><input type="text" id="NewUserName"></td>' +
+                '<td class="td10"><select>' +
+                '<?php
+                    $sql = $zbp->db->sql->Select($GLOBALS['table']['plugin_oauth2_group'], '*');
+                    $array = $zbp->GetListCustom($GLOBALS['table']['plugin_oauth2_group'], $GLOBALS['datainfo']['plugin_oauth2_group'], $sql);
+                    foreach ($array as $key => $reg) {
+                        if ($reg->status != "已删除") {
+                            $gid = $reg->gid;
+                            echo <<<EOF
+<option value="$gid">{$reg->gname}</option>
+EOF;
+                        }
+                    }
+                    ?>' +
+                '</select></td>' +
                 '<td class="td15"></td>' +
-                '<td class="tdCenter"><div  style="width: 107px">'+
-                '<input type="text" style="width: 66px;" id="NewUserInvcode' + newnum.toString() + '" />'+
+                '<td class="tdCenter"><div  style="width: 107px">' +
+                '<input type="text" style="width: 66px;" id="NewUserInvcode" />' +
                 '<a style="cursor: pointer;"><img style="margin-bottom: -11px;margin-right: -10px;" src="./image/gencode.png" alt="重新生成邀请码" title="重新生成邀请码" onclick="getRandomString(this.parentNode.parentNode.firstChild.id)" width="32" >' +
                 '</div></td>' +
                 '<td class="td20 tdCenter"></td>' +
                 '<td class="td5 tdCenter"><select><option value="待激活">待激活</option><option value="正常">正常</optionval></select></td>' +
                 '<td class="td10 tdCenter">' +
-                '<a class="button">' +
-                '<img onclick="SubmitNewLine(this)" ' +
-                'src="../../../zb_system/image/admin/ok.png" alt="提交" title="提交" width="16"></a>' +
-                '&nbsp;&nbsp;&nbsp;&nbsp;' +
-                '<a class="button">' +
-                '<img onclick="DelNewLine(this)" src="../../../zb_system/image/admin/delete.png" alt="删除" title="删除" width="16">' +
-                '</a>' +
-                '</td>' +
-                '</tr>');
+                '<a href="#" class="button"><img src = "../../../zb_system/image/admin/tick.png" alt = "提交" title = "提交" width = "16" ></a>&nbsp;&nbsp;&nbsp;' +
+                '<a href="#" class="button"><img src = "../../../zb_system/image/admin/delete.png" alt = "删除" title = "删除" width = "16" ></a> </td></tr >');
             if (newnum > 1) {
-                $("[colspan='7']").html('<center>' +
-                    '<button class="lbt" type="button" onclick=""> 提交全部 </button></center>');
+                $("[colspan='7']").html('<button class="lbt" type="button"> 提交全部 </button>');
             }
             getRandomString('NewUserInvcode' + newnum.toString());
         }
+
+
         function getRandomString(id) {
             var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678'; // 默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1
             var maxPos = $chars.length;
             var Invcode = '';
-            for (i = 0; i < 6; i++) {
+            for (var i = 0; i < 6; i++) {
                 Invcode += $chars.charAt(Math.floor(Math.random() * maxPos));
             }
             $.ajax({
