558ead23ce9aa734f50e63fa2e4f289f302cda5e
cxgreat2014 <fwy1998@gmail.com>
2016-10-05 17:14:20 +0800

    rm pua

--- include (2).php
+++
@@ -1,7 +0,0 @@
-<?php
-#注册插件
-RegisterPlugin("VisitorUserCenter","ActivePlugin_VisitorUserCenter");
-
-function ActivePlugin_VisitorUserCenter() {}
-function InstallPlugin_VisitorUserCenter() {}
-function UninstallPlugin_VisitorUserCenter() {}
--- include.php
+++ include.php
@@ -1,5 +1,5 @@
 <?php
-if(!defined('UC_path')) {
+if (!defined('UC_path')) {
     define('UC_path', preg_replace("/(\/zb_users\/plugin\/VisitorUserCenter\/).*/is", "\\1", str_replace('\\', '/', __FILE__)) . "/");
     define('site_path', preg_replace("/(\/zb_users\/plugin\/VisitorUserCenter\/).*/is", "", str_replace('\\', '/', __FILE__)) . "/");
 }
@@ -9,13 +9,15 @@ require_once UC_path . 'System/loadmod.php';
 #注册插件
 RegisterPlugin("vuc", "ActivePlugin_VisitorUserCenter");
 
-function ActivePlugin_VisitorUserCenter() {
+function ActivePlugin_VisitorUserCenter()
+{
     Add_Filter_Plugin('Filter_Plugin_Html_Js_Add', 'vuc_Js_Add');
     Add_Filter_Plugin('Filter_Plugin_Index_Begin', 'vuc_Index_Begin');
     Add_Filter_Plugin('Filter_Plugin_Logs', 'vuc_Logs');
 }
 
-function vuc_Js_Add() {
+function vuc_Js_Add()
+{
     global $zbp;
     $action = 'root';
     if ($zbp->CheckRights($action)) {
@@ -23,17 +25,20 @@ function vuc_Js_Add() {
     }
 }
 
-function vuc_Index_Begin() {
+function vuc_Index_Begin()
+{
 
 }
 
-function vuc_Logs($log, $e) {
+function vuc_Logs($log, $e)
+{
     $vuc = new VUC();
     $vuc->OutputLog('0', 'system_log' . ($e == true ? '' : '_err'), $log);
 }
 
-function ReNewSiteCtrlJSFile() {
-    $vuc=new VUC();
+function ReNewSiteCtrlJSFile()
+{
+    $vuc = new VUC();
     $SiteCtrljs = fopen("../../User/js/SiteCtrl.js", "w") or die("Unable to open file!");
 
     if ($vuc->GetConfig('normenu'))
@@ -85,15 +90,21 @@ $("body").html("<div style=\"position:fixed;top:0;left:0;width:100%;height:100%;
     fclose($SiteCtrljs);
 }
 
+function vuc_genstr($length = 16)
+{
+// 密码字符集，可任意添加你需要的字符
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
 
-
-
-function InstallPlugin_VisitorUserCenter() {
-    vuc_CreatTable();
-    vuc_init();
+        $password .= $chars[mt_rand(0, strlen($chars) - 1)];
+    }
+    return $password;
 }
 
-function vuc_CreatTable() {
+
+function InstallPlugin_VisitorUserCenter()
+{
     global $zbp;
     //数据库检测与创建
     $s = '';
@@ -111,9 +122,7 @@ function vuc_CreatTable() {
         $s .= $zbp->db->sql->CreateTable($GLOBALS['table']['vuc_config'], $GLOBALS['datainfo']['vuc_config']) . ';';
     }
     $zbp->db->QueryMulit($s);
-}
-
-function vuc_init() {
+    //初始化
     $vuc = new VUC();
     $vuc->SetConfig('normenu', '0');
     $vuc->SetConfig('noselect', '0');
@@ -124,8 +133,42 @@ function vuc_init() {
     $vuc->SetConfig('closetips', '网站正在维护，请稍后再访问');
     $vuc->SetConfig('sitehost', $_SERVER['HTTP_HOST']);
     $vuc->SetConfig('siteprocted', '0');
+    //创建加密证书实行加密通讯
+    $dn = array(
+        "countryName" => "CN",
+        "stateOrProvinceName" => "Beijing",
+        "localityName" => "Beijing",
+        "organizationName" => "MySelf",
+        "organizationalUnitName" => "Whatever",
+        "commonName" => "mySelf",
+        "emailAddress" => "user@domain.com"
+    );
+
+    $privkeypass = 'cxg2014.bit'; //私钥密码
+    $numberofdays = 365;     //有效时长
+    $ckfn = vuc_genstr();
+    $cerpath = $ckfn.".cer"; //生成证书路径
+    $pfxpath = $ckfn.".pfx"; //密钥文件路径
+    $vuc->SetConfig('RSA', $ckfn);
+
+
+    //生成证书
+    $privkey = openssl_pkey_new();
+    $csr = openssl_csr_new($dn, $privkey);
+    $sscert = openssl_csr_sign($csr, null, $privkey, $numberofdays);
+    openssl_x509_export($sscert, $csrkey); //导出证书$csrkey
+    openssl_pkcs12_export($sscert, $privatekey, $privkey, $privkeypass); //导出密钥$privatekey
+    //生成证书文件
+    $fp = fopen($cerpath, "w");
+    fwrite($fp, $csrkey);
+    fclose($fp);
+    //生成密钥文件
+    $fp = fopen($pfxpath, "w");
+    fwrite($fp, $privatekey);
+    fclose($fp);
 }
 
 
-function UninstallPlugin_VisitorUserCenter() {
+function UninstallPlugin_VisitorUserCenter()
+{
 }
